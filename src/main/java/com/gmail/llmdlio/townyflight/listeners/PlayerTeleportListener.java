d47ec199e9e363b7e1a47a8cbd89fded927a2ee0
Jsinco <jonah@jsinco.dev>
2026-02-03 02:47:57 -0500

    Port to CanvasMC

--- src/main/java/com/gmail/llmdlio/townyflight/listeners/PlayerTeleportListener.java
+++ src/main/java/com/gmail/llmdlio/townyflight/listeners/PlayerTeleportListener.java
@@ -1,11 +1,11 @@
 package com.gmail.llmdlio.townyflight.listeners;
 
+import io.canvasmc.canvas.event.EntityPostTeleportAsyncEvent;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 import com.gmail.llmdlio.townyflight.TownyFlightAPI;
@@ -15,11 +15,14 @@ import com.palmergames.bukkit.towny.object.Resident;
 public class PlayerTeleportListener implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR)
-	private void playerTeleports(PlayerTeleportEvent event) {
-		if (!aTeleportCauseThatMatters(event.getCause()))
-			return;
+	private void playerTeleports(EntityPostTeleportAsyncEvent event) {
+        if (!(event.getEntity() instanceof Player player))
+            return;
 
-		Player player = event.getPlayer();
+		if (!aTeleportCauseThatMatters(event.getCause())) {
+            return;
+        }
+
 		if (player.hasPermission("townyflight.bypass") 
 				|| !player.getAllowFlight()
 				|| flightAllowedDestination(player, event.getTo())) {
@@ -31,7 +34,7 @@ public class PlayerTeleportListener implements Listener {
 
 	private boolean aTeleportCauseThatMatters(TeleportCause teleportCause) {
 		return teleportCause == TeleportCause.PLUGIN || teleportCause == TeleportCause.COMMAND ||
-				teleportCause == TeleportCause.ENDER_PEARL || teleportCause == TeleportCause.CHORUS_FRUIT; // TODO: change over when 1.21.4 and older support is dropped.
+				teleportCause == TeleportCause.ENDER_PEARL || teleportCause == TeleportCause.CONSUMABLE_EFFECT; // TODO: change over when 1.21.4 and older support is dropped.
 	}
 
 	private boolean flightAllowedDestination(Player player, Location loc) {
